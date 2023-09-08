13f88e2aa72facdc9c0face19c8de095776eefdc
Oyinkansola <tellaoyinkansola09@gmail.com>
2023-09-09 01:23:42 +0200

    new commits

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const myDate = new Date();
-const utcMilliseconds = myDate.getUTCMilliseconds();
-document.getElementById("currentUTCTime").innerHTML=(utcMilliseconds)
+function UtcTime(){
+        const myDate = new Date();
+        const utcMilliseconds = myDate.getTime();
+        document.getElementById("currentUTCTime").innerHTML= utcMilliseconds;
+        
+        const daysOfWeek =["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        const day = daysOfWeek[myDate.getDay()];
+        document.getElementById("currentDayOfTheWeek").innerHTML = day;
+      
+}
 
-const daysOfWeek =["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-const day = daysOfWeek[myDate.getDay()];
-document.getElementById("currentDayOfTheWeek").innerHTML = (day)
+UtcTime();
