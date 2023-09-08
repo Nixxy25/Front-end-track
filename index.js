433b1813b1ab85dc165d0957c4de99bb0cec9c50
Oyinkansola <tellaoyinkansola09@gmail.com>
2023-09-09 01:47:44 +0200

    commits

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function UtcTime(){
         const myDate = new Date();
         const utcMilliseconds = myDate.getTime();
-        document.getElementById("currentUTCTime").innerHTML= utcMilliseconds;
+        const timeNow = utcMilliseconds.toString();
+        document.getElementById("currentUTCTime").innerHTML= timeNow;
         
         const daysOfWeek =["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
         const day = daysOfWeek[myDate.getDay()];
