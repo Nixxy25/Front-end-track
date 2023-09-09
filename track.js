c58b422b4bb4214a4c3b4808d3228d5f1040bb2f
Oyinkansola <tellaoyinkansola09@gmail.com>
2023-09-09 19:29:13 +0200

    changes

--- track.js
+++ track.js
@@ -1,24 +1,23 @@
-function UtcTime(){
-        const myDate = new Date();
-        const daysOfWeek =[
-                "Sunday", 
-                "Monday",
-                "Tuesday",
-                "Wednesday",
-                "Thursday",
-                "Friday",
-                "Saturday"
-        ];
+function utcMilliseconds(){
+    const myDate = new Date();
 
+    const daysOfWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ]
 
-        const day = daysOfWeek[myDate.getDay()];
-        document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = day;
+    const day = daysOfWeek[myDate.getUTCDay];
+    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = day;
 
-        const utcTime = myDate.toUTCString().split(" ")[4];
-        document.querySelector('[data-testid="currentUTCTime"]').textContent = utcTime;
-      
+    const utcTime = myDate.toUTCString().split(" ")[4];
+    document.querySelector('[data-testid="currentUTCTime"]').textContent= utcTime;
 }
 
-UtcTime();
+utcMilliseconds();
 
-setInterval(UtcTime, 1000)
+setInterval(utcMilliseconds, 1000)
