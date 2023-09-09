715d63c68d681334e4234b6e4618a892e400d12d
Oyinkansola <tellaoyinkansola09@gmail.com>
2023-09-09 09:04:59 +0200

    new changes 1

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function UtcTime(){
         const myDate = new Date();
-        const utcMilliseconds = myDate.getTime();
-        document.getElementById("currentUTCTime").innerHTML= utcMilliseconds;
+        // const utcMilliseconds = myDate.getTime();
+        const utcTime = myDate.toUTCString().split(" ")[4];
+        document.getElementById("currentUTCTime").innerHTML= utcTime;
         
         const daysOfWeek =["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
         const day = daysOfWeek[myDate.getDay()];
@@ -10,3 +11,5 @@ function UtcTime(){
 }
 
 UtcTime();
+
+setInterval(UtcTime, 1000)
